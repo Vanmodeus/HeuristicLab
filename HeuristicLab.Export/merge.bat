9ceed828fea73496c64bcfadb7b47e034fa3ac74
fHolz <vanmodeus@gmail.com>
2022-04-02 13:02:44 +0200

    Added merge script / ILMerge

---
+++ HeuristicLab.Export/merge.bat
@@ -0,0 +1,19 @@
+﻿REM --Start script--
+DEL /F/Q/S export > NUL
+RMDIR export
+MKDIR export
+XCOPY *.dll export
+XCOPY *.pdb export
+XCOPY *.exe export
+
+cd export
+
+ILMerge.exe HeuristicLab.*.dll Microsoft.*.dll /out:HeuristicLab.Merged.dll /wildcards /allowDup
+RENAME HeuristicLab.Merged.dll a.b
+RENAME HeuristicLab.Merged.dll.pdb c.d
+
+DEL HeuristicLab.*.dll
+DEL Microsoft.*.dll
+
+RENAME a.b HeuristicLab.Merged.dll 
+RENAME c.d HeuristicLab.Merged.dll.pdb 
