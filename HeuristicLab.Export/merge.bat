d971fa2c6d9536da4cbec26ffc1779bc7e311146
fHolz <vanmodeus@gmail.com>
2022-04-02 13:12:25 +0200

    Fixed merge script

--- HeuristicLab.Export/merge.bat
+++ HeuristicLab.Export/merge.bat
@@ -10,10 +10,10 @@ cd export
 
 ILMerge.exe HeuristicLab.*.dll Microsoft.*.dll /out:HeuristicLab.Merged.dll /wildcards /allowDup
 RENAME HeuristicLab.Merged.dll a.b
-RENAME HeuristicLab.Merged.dll.pdb c.d
+RENAME HeuristicLab.Merged.pdb c.d
 
 DEL HeuristicLab.*.dll
 DEL Microsoft.*.dll
 
 RENAME a.b HeuristicLab.Merged.dll 
-RENAME c.d HeuristicLab.Merged.dll.pdb 
+RENAME c.d HeuristicLab.Merged.pdb 
