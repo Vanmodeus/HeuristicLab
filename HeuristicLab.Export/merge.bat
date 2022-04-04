166e104b745d488cf3523e2b317d8f08b0051635
fHolz <vanmodeus@gmail.com>
2022-04-04 21:23:10 +0200

    Adapted merge script to clean up default libraries

--- HeuristicLab.Export/merge.bat
+++ HeuristicLab.Export/merge.bat
@@ -7,13 +7,22 @@ XCOPY *.pdb export
 XCOPY *.exe export
 
 cd export
+DEL System.*.dll
+DEL Microsoft.*.dll
+DEL System.*.pdb
+DEL Microsoft.*.pdb
 
-ILMerge.exe HeuristicLab.*.dll /out:HeuristicLab.Merged.dll /wildcards /allowDup
+cd ..
+ILMerge.exe export/HeuristicLab.*.dll /out:export/HeuristicLab.Merged.dll /wildcards /allowDup
+cd export
+
 RENAME HeuristicLab.Merged.dll a.b
 RENAME HeuristicLab.Merged.pdb c.d
 
 DEL HeuristicLab.*.dll
 DEL HeuristicLab.*.pdb
+DEL ILMerge.exe
 
 RENAME a.b HeuristicLab.Merged.dll 
 RENAME c.d HeuristicLab.Merged.pdb 
+cd ..
